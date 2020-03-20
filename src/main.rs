2e78d09986dc075817f5fa8035cf949371821e11
adriano <jadrian.osoriop@gmail.com>
2020-03-19 19:04:12 -0600

    Add listenfd dependency for auto reloading

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder , get};
+use listenfd::ListenFd;
 
 #[get{"/"}]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+    HttpResponse::Ok().body("Hello World!!!")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new().service(index)
-    })
-        .bind("127.0.0.1:5000")?.run().await
+    });
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind("127.0.0.0:5000")?,
+    };
+    
+    server.run().await
 }
