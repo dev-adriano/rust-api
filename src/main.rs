73d9040d967cafa534f9b70e4b6f7b72d8ec4071
adriano <jadrian.osoriop@gmail.com>
2020-03-20 11:53:52 -0600

    Add support for environment variables and logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate log;
+
 use actix_web::{App, HttpResponse, HttpServer, Responder , get};
 use listenfd::ListenFd;
+use dotenv::dotenv;
+use std::env;
 
 #[get{"/"}]
 async fn index() -> impl Responder {
@@ -8,6 +13,9 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(|| {
         App::new().service(index)
@@ -15,8 +23,13 @@ async fn main() -> std::io::Result<()> {
 
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
-        None => server.bind("127.0.0.0:5000")?,
+        None => {
+            let host = env::var("HOST").expect("Host not set");
+            let port = env::var("PORT").expect("Port not set");
+            server.bind(format!("{}:{}", host, port))?
+        }
     };
     
+    info!("Start server");
     server.run().await
 }
